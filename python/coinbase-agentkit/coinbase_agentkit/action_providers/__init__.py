ac636e441941e803318bce76789f7b54c35bef04
Fernando Campos <fernandofcampos@gmail.com>
2025-02-17 19:57:58 -0300

    Adds Allora Network price inference to the CDP AgentKit.

--- python/coinbase-agentkit/coinbase_agentkit/action_providers/__init__.py
+++ python/coinbase-agentkit/coinbase_agentkit/action_providers/__init__.py
@@ -20,6 +20,7 @@ from .twitter.twitter_action_provider import TwitterActionProvider, twitter_acti
 from .wallet.wallet_action_provider import WalletActionProvider, wallet_action_provider
 from .weth.weth_action_provider import WethActionProvider, weth_action_provider
 from .wow.wow_action_provider import WowActionProvider, wow_action_provider
+from .allora.allora_action_provider import AlloraActionProvider, allora_action_provider
 
 __all__ = [
     "Action",
@@ -49,4 +50,6 @@ __all__ = [
     "weth_action_provider",
     "WowActionProvider",
     "wow_action_provider",
+    "AlloraActionProvider",
+    "allora_action_provider",
 ]
