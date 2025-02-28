ed057e2a22bb390ff29adfc3da36b9996e7248bf
Christopher Gerber <csgerber@gmail.com>
2025-02-28 09:39:07 -0800

    python partial format revert

--- python/coinbase-agentkit/coinbase_agentkit/wallet_providers/cdp_wallet_provider.py
+++ python/coinbase-agentkit/coinbase_agentkit/wallet_providers/cdp_wallet_provider.py
@@ -109,6 +109,7 @@ class CdpWalletProvider(EvmWalletProvider):
                 else 1
             )
 
+
         except ImportError as e:
             raise ImportError(
                 "Failed to import cdp. Please install it with 'pip install cdp-sdk'."
