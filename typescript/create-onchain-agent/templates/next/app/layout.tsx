fda34eda5f583a18bec2075a0dbed89ced580031
CarsonRoscoe <carsonroscoe7@gmail.com>
2025-03-10 11:58:31 -0400

    chore: reverted img change

--- typescript/create-onchain-agent/templates/next/app/layout.tsx
+++ typescript/create-onchain-agent/templates/next/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Image from "next/image";
 import "./globals.css";
 
 /**
@@ -43,12 +42,10 @@ export default function RootLayout({
 
         {/* Footer (Fixed Height) */}
         <footer className="py-4 text-center text-gray-500 dark:text-gray-400 flex-none">
-          <Image
+          <img
             src="https://images.ctfassets.net/q5ulk4bp65r7/3TBS4oVkD1ghowTqVQJlqj/2dfd4ea3b623a7c0d8deb2ff445dee9e/Consumer_Wordmark.svg"
             alt="Coinbase"
             className="h-6 mx-auto mb-2"
-            width={96}
-            height={24}
           />
           <div className="mt-2">
             <a
